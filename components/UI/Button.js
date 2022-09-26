4990a9e228fe6e7f71eb84d63be817d182ad7320
Murad <murad.aliyev.13.murad@gmail.com>
2022-09-26 16:20:50 +0400

    Button style changed

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -4,21 +4,22 @@ import { View, StyleSheet, Pressable, Text } from 'react-native';
 const Button = ({children,backgroundColor,color,onPress,borderColor}) => {
     
     return (
-        <View >
-            <Pressable onPress={onPress}>
+            <Pressable style={{flex:1}} onPress={onPress}>
                 <View style={[styles.container,{backgroundColor:backgroundColor,borderColor:borderColor}]}>
                     <Text style={{color:color,textAlign:'center'}}>{children}</Text>
                 </View>
             </Pressable>
-        </View>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
+        flex:1,
         paddingVertical:12,
         paddingHorizontal:12,
         borderRadius:10,
+        justifyContent:'center',
+        alignItems:'center',
     }
 });
 
