8fd5d3ec748ac13d5bea13bdad8f3d95e55c98be
Murad <murad.aliyev.13.murad@gmail.com>
2022-09-26 13:30:27 +0400

    Login and Register page created

--- app/App.js
+++ app/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import {StyleSheet, Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
 
 const App = () => {
+
+  const BottomTab=()=>{
+
+    return(
+      <BottomTabs.Navigator>
+        <BottomTabs.Screen name='Login'></BottomTabs.Screen>
+        <BottomTabs.Screen name='Register' component={}></BottomTabs.Screen>
+      </BottomTabs.Navigator>
+    )
+  }
+
   return(
-    <View>
-      <Text>Salam</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Main' component={BottomTab}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 };
 
