8cd3162973eec9fc34e72dacb1b8a569ab3a226f
Murad <murad.aliyev.13.murad@gmail.com>
2022-10-20 14:36:28 +0400

    Share added

--- app/components/Order/Order.js
+++ app/components/Order/Order.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import colors from '../../config/colors';
 import ProductDetail from '../ProductDetail/ProductDetail';
+import Button from '../UI/Button';
 
-const Order = () => {
+const Order = ({navigation}) => {
     
     return (
         <View style={styles.container}>
-            <ProductDetail></ProductDetail>
+            <Button onPress={()=>{
+                navigation.navigate('ProductDetail');
+            }} color={colors.white} backgroundColor={colors.blue}>Press me</Button>
         </View>
     )
 };
---
+++ app/components/UI/AddedButton.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, StyleSheet, Pressable, Text} from 'react-native';
+import px from '../../assets/utility/dimension';
+
+const AddedButton = ({children, backgroundColor, color, onPress, borderColor}) => {
+  return (
+    <Pressable style={{flex: 1}} onPress={onPress}>
+      <View
+        style={[
+          styles.container,
+          {backgroundColor: backgroundColor, borderColor: borderColor},
+          borderColor && {borderWidth: 1},
+        ]}>
+        {children}
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: px(12),
+    paddingHorizontal: px(16),
+    borderRadius: px(12),
+  },
+});
+
+export default AddedButton;
