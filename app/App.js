e4836e044d8f1643dccbab6bc01e277f9be40136
nizamiqarayev <nizamiqarayev59@gmail.com>
2022-10-24 16:10:02 +0400

    some warning messages added to register

--- app/App.js
+++ app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {StyleSheet, Text, View} from 'react-native';
+import {LogBox, StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
 import BottomTabsenComponent from './stack/BottomTabsenComponent';
@@ -10,6 +10,13 @@ import { store } from './store/store';
 //comment
 
 const App = () => {
+  LogBox.ignoreLogs([
+    'VirtualizedLists should never be nested inside plain ScrollViews with the same orientation because it can break windowing and other functionality - use another VirtualizedList-backed container instead.',
+    `[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!`,
+    `[mobx] Out of bounds read`,
+    'Non-serializable values were found in the navigation state',
+    'Task orphaned',
+  ]);
   const BottomTab = () => {
     return (
       <BottomTabsenComponent />
