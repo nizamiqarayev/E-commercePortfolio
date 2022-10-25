c9c7b27de038ccf3f2cf5512b5e13e771c76efcc
Kanan Islamzada <islamzade.kenan1@gmail.com>
2022-10-25 21:56:11 +0400

    c

--- ios/eCommerce/AppDelegate.h
+++ ios/eCommerce/AppDelegate.h
@@ -1,10 +1,6 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
-#import "RNQuickActionManager.h"
 
-- (void)application:(UIApplication *)application performActionForShortcutItem:(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL succeeded)) completionHandler {
-  [RNQuickActionManager onQuickActionPress:shortcutItem completionHandler:completionHandler];
-}
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
