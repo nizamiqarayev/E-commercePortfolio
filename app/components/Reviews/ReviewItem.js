c5903aa5bd8b7d6f858e0e11ac2a6e4df8de349b
Murad <murad.aliyev.13.murad@gmail.com>
2022-10-30 22:54:07 +0400

    Review is Finished

--- app/components/Reviews/ReviewItem.js
+++ app/components/Reviews/ReviewItem.js
@@ -7,22 +7,23 @@ import colors from '../../config/colors';
 import Stars from './Stars';
 
 const ReviewItem = ({data}) => {
+
     
     return (
         <View style={styles.container}>
             <View style={{marginRight:px(15)}}>
-                <Image style={styles.image} source={{uri:data.coverPhoto}}/>
+                <Image style={styles.image} source={{uri:data.profileImage}}/>
             </View>
             <View style={{flex:1}}>
                 <View style={{flexDirection:'row',justifyContent:'space-between',marginBottom:px(7)}}>
-                    <Text style={styles.name} >{data.title}</Text>
-                    <Text style={styles.date}>{getdate(data.date)}</Text>
+                    <Text style={styles.name} >{data.username}</Text>
+                    <Text style={styles.date}>{getdate(data.created)}</Text>
                 </View>
                 <View style={{marginBottom:px(9)}}>
-                    <Stars key={Math.random()} filled={data.rating}/>
+                    <Stars key={Math.random()} filled={data.starCount}/>
                 </View>
                 <View>
-                    <Text style={styles.description}>{data.description}</Text>
+                    <Text style={styles.description}>{data.review}</Text>
                 </View>
             </View>
         </View>
